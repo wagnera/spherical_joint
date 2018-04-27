0acf03d136e3fa94b2c85df985f5259bc7e755da
wagnera <wagnera@vt.edu>
2018-04-26 23:49:01 -0400

    adding model for rviz

--- src/live_measure.py
+++ src/live_measure.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.externals import joblib
+from geometry_msgs.msg import TransformStamped
+import tf2_ros
 
-class spherical_calibration:
+class spherical_measure:
 	def __init__(self):
 		self.serial_buffer=""
 		rospy.init_node("Measurement_Node")
@@ -13,11 +15,13 @@ class spherical_calibration:
 		self.model1=joblib.load('axis1_model.sav')
 		self.model2=joblib.load('axis2_model.sav')
 		rospy.loginfo("Models Loaded")
+		self.tf_br = tf2_ros.TransformBroadcaster()
 
 	def read_serial(self):
 		read=self.ser.read(self.ser.inWaiting())
 		if len(read) > 0:
 			self.serial_buffer += read
+			print(self.serial_buffer)
 		else:
 			return
 		while self.serial_buffer.find('\r\n') > 0:
@@ -33,10 +37,20 @@ class spherical_calibration:
 				#print(theta_1)
 				theta_2=self.model2.predict(measurement)[0]
 				print(theta_1,theta_2)
+				t = TransformStamped()
+				t.header.stamp = rospy.Time.now()
+				t.header.frame_id='base_link'
+				t.child_frame_id='cup'
+				q=eul2quat(math.radians(theta_1),math.radians(theta_2),0)
+				t.transform.rotation.x=q[0]
+				t.transform.rotation.y=q[1]
+				t.transform.rotation.z=q[2]
+				t.transform.rotation.w=q[3]
+				self.tf_br.sendTransform(t)
 
 	def spinner(self):
 		while not rospy.is_shutdown():
 			self.read_serial()
 
-a=spherical_calibration()
+a=spherical_measure()
 a.spinner()
